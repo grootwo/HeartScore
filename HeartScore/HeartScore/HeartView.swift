99ab79aaef3bc8ef8037930632331967368202a9
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-10-23 16:45:40 +0900

    feat: extract game center view

---
+++ HeartScore/HeartScore/HeartView.swift
@@ -0,0 +1,32 @@
+//
+//  HeartView.swift
+//  HeartScore
+//
+//  Created by Groo on 10/23/24.
+//
+
+import SwiftUI
+
+struct HeartView: View {
+    @State private var heartCount: Int = 0
+    var body: some View {
+        VStack(spacing: 30) {
+            Button(action: {
+                heartCount += 1
+            }, label: {
+                Image(systemName: "heart.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(RadialGradient(gradient: Gradient(colors: [.red, .pink]), center: .center, startRadius: 50, endRadius: 100))
+            })
+            Text("Your Heart Score: \(heartCount)")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HeartView()
+}
