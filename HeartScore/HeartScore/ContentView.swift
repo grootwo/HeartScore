f44977fa12d30d311c346a4d1201885f798b07a6
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-10-23 16:00:10 +0900

    feat: create heart button and score

--- HeartScore/HeartScore/ContentView.swift
+++ HeartScore/HeartScore/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var heartCount: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Button(action: {
+                heartCount += 1
+            }, label: {
+                Image(systemName: "heart.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(RadialGradient(gradient: Gradient(colors: [.red, .pink]), center: .center, startRadius: 50, endRadius: 100))
+            })
+            Text("Your Heart Score: \(heartCount)")
+                .font(.largeTitle)
         }
         .padding()
     }
