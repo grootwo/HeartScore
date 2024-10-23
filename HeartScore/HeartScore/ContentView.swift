efd74d4179331dbf84fc252189d098b75774e36c
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-10-23 15:51:24 +0900

    feat: create HeartScore project

---
+++ HeartScore/HeartScore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeartScore
+//
+//  Created by Groo on 10/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeartScore/HeartScore/HeartScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartScoreApp.swift
+//  HeartScore
+//
+//  Created by Groo on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct HeartScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
